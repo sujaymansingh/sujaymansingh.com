6cdbd35a4d2697501c71c42749e512d7fcc1aedb
Sujay Mansingh <sujay@hoju-jr.local>
2013-01-15 22:18:31 +0000

    need this for run_gunicorn command

--- src/webapp/webapp/settings/core.py
+++ src/webapp/webapp/settings/core.py
@@ -117,6 +117,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'countdown',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
