98c530397022e9a6d2349aae738e9e86c2b58dfa
Sujay Mansingh <sujay@hoju-jr.local>
2013-01-15 22:04:40 +0000

    setting for amazon.

---
+++ src/webapp/webapp/settings/env_aws.py
@@ -0,0 +1,8 @@
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/opt/sujaymansingh.com/static/'
+
+COUNTDOWN_DATA_DIR = '/opt/sujaymansingh.com/countdown_data/'
+COUNTDOWN_PHOTO_BASE = '/countdown/'
